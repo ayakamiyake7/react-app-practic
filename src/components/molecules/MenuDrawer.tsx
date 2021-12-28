538521e5d4ef595d88634cee7453981236ae008e
Ayaka Miyake <ayakamiyake7@gmail.com>
2021-12-28 15:44:37 +0000

    Add header.

--- src/components/molecules/MenuDrawer.tsx
+++ src/components/molecules/MenuDrawer.tsx
@@ -10,18 +10,33 @@ import {
 type Props = {
   onClose: () => void;
   isOpen: boolean;
+  onClickHome: () => void;
+  onClickUserManagement: () => void;
+  onClickSetting: () => void;
 };
 
 export const MenuDrawer: VFC<Props> = memo((props) => {
-  const { onClose, isOpen } = props;
+  const {
+    onClose,
+    isOpen,
+    onClickHome,
+    onClickUserManagement,
+    onClickSetting,
+  } = props;
   return (
     <Drawer placement="left" size="xs" onClose={onClose} isOpen={isOpen}>
       <DrawerOverlay>
         <DrawerContent>
           <DrawerBody p={0} bg="gray.100">
-            <Button w="100%">TOP</Button>
-            <Button w="100%">User</Button>
-            <Button w="100%">Setting</Button>
+            <Button w="100%" onClick={onClickHome}>
+              TOP
+            </Button>
+            <Button w="100%" onClick={onClickUserManagement}>
+              User
+            </Button>
+            <Button w="100%" onClick={onClickSetting}>
+              Setting
+            </Button>
           </DrawerBody>
         </DrawerContent>
       </DrawerOverlay>
