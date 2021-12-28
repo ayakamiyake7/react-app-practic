07e7210e786369255a4abb37d7fd91965400878a
Ayaka Miyake <ayakamiyake7@gmail.com>
2021-12-28 14:21:45 +0000

    Add header.

---
+++ src/components/organisms/layout/Header.tsx
@@ -0,0 +1,68 @@
+import {
+  Box,
+  Button,
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerBody,
+  Flex,
+  Heading,
+  IconButton,
+  Link,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { memo, VFC } from "react";
+import { HamburgerIcon } from "@chakra-ui/icons";
+
+export const Header: VFC = memo(() => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Flex
+        as="nav"
+        bg="teal.500"
+        color="gray.50"
+        align="center"
+        justify="space-between"
+        padding={{ base: 3, md: 5 }}
+      >
+        <Flex align="center" as="a" mr={8} _hover={{ cursor: "pointer" }}>
+          <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
+            App
+          </Heading>
+        </Flex>
+        <Flex
+          align="center"
+          fontSize="sm"
+          flexGrow={2}
+          display={{ base: "none", md: "flex" }}
+        >
+          <Box pr={4}>
+            <Link>User</Link>
+          </Box>
+          <Link>Setting</Link>
+        </Flex>
+        <IconButton
+          aria-label="Menu button"
+          icon={<HamburgerIcon />}
+          size="sm"
+          variant="unstyled"
+          display={{ base: "block", md: "none" }}
+          onClick={onOpen}
+        />
+      </Flex>
+
+      <Drawer placement="left" size="xs" onClose={onClose} isOpen={isOpen}>
+        <DrawerOverlay>
+          <DrawerContent>
+            <DrawerBody p={0} bg="gray.100">
+              <Button w="100%">TOP</Button>
+              <Button w="100%">User</Button>
+              <Button w="100%">Setting</Button>
+            </DrawerBody>
+          </DrawerContent>
+        </DrawerOverlay>
+      </Drawer>
+    </>
+  );
+});
