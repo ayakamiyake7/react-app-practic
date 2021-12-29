97a5861130c08766ade457f97a4c8a40eb59db48
Ayaka Miyake <ayakamiyake7@gmail.com>
2021-12-29 16:30:29 +0000

    Add user page function.

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,35 @@
-import { memo, VFC } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+
+import { Center, Wrap, WrapItem } from "@chakra-ui/layout";
+import { Spinner } from "@chakra-ui/spinner";
+import { memo, useEffect, VFC } from "react";
+import { useAllUsers } from "../../hooks/useAllUsers";
+import { UserCard } from "../organisms/user/UserCard";
 
 export const UserManagement: VFC = memo(() => {
-  return <p>User Page</p>;
+  const { getUsers, users, loading } = useAllUsers();
+
+  useEffect(() => getUsers, []);
+
+  return (
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          {users.map((user) => (
+            <WrapItem key={user.id} mx="auto">
+              <UserCard
+                imageUrl="https://source.unsplash.com/random"
+                userName={user.username}
+                fullName={user.name}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
+  );
 });
